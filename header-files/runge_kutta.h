ccfa95094eb4fc3ee7dd6a8a225903b49f215773
AnnaPaulish <apaulish99@gmail.com>
2021-12-08 18:52:37 +0100

    runge_kutta correction

--- header-files/runge_kutta.h
+++ header-files/runge_kutta.h
@@ -1,12 +1,10 @@
-//
-// Created by Leonhard Driever on 04.12.21.
-//
+
 
 #ifndef ODE_SOLVER_PROJECT_RUNGE_KUTTA_H
 #define ODE_SOLVER_PROJECT_RUNGE_KUTTA_H
 
 #include "ode_parent.h"
-#include 'setup.h'
+#include "setup.h"
 
 class RungeKutta: public ODE {
 
