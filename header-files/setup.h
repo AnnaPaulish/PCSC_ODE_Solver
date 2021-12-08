6f1b9a362c377c876bb40f8d6851c3a81b3f2a60
AnnaPaulish <apaulish99@gmail.com>
2021-12-08 16:55:37 +0100

    setup update

--- header-files/setup.h
+++ header-files/setup.h
@@ -4,31 +4,44 @@
 
 #ifndef ODE_SOLVER_PROJECT_SETUP_H
 #define ODE_SOLVER_PROJECT_SETUP_H
-
-#include "Eigen/Dense"
-
-
-#include <string> // TODO Delete if no longer necessary
-
-namespace E = Eigen;
-
 class SetUp {
 public:
-    int N = 50;
-    double dt = 0.1;
+    // Constructor and destructor
+    Set(std::string file_name);
 
+    int N = 5; // total number of steps N
+    double dt = 0.1; // time step dt
     double x = 0.0;
+    Eigen::ArrayXd t = Eigen::ArrayXd::LinSpaced(N+1, 0, N*dt);
+    Eigen::ArrayXd y = Eigen::ArrayXd::Ones(N);
 
-    std::string method = "ForwardEuler";
+    int polynomial_degree = 1; //
+    Eigen::ArrayXd poly_coefs_y = Eigen::ArrayXd::Ones(polynomial_degree+1);
 
-    E::ArrayXd t = E::ArrayXd::LinSpaced(N+1, 0, N*dt);;
-
-    E::ArrayXd y = E::ArrayXd::Ones(N);
     int sampling_frequency = 2;
 
-    double RHS(double a, double b) {
-        return 1;
-    }
+    double RHS(double y_value, double t_value, double x_value);
+
+    std::string output_path; // Output File Path: path/my_output.dat
+    bool console_output; // True if we want to get the result in Console
+    //bool console_input;
+    std::string method; // solving method (string without quotation marks)
+
+    bool testing; // True if we want to test
+    // methods
+    void read_settings(std::string settings_file_name);
+    void read_command_line();
+    void read_file(std::string settings_file_name);
+    void read_console();
+
+private:
+    double t_0; // initial time
+    double y_0; // initial y value
+
+    void make_t();
+
 };
+
+
 
 #endif //ODE_SOLVER_PROJECT_SETUP_H
--- source-files/setup.cpp
+++ source-files/setup.cpp
@@ -1,4 +1,110 @@
 //
 // Created by Leonhard Driever on 03.12.21.
 //
+#include <iostream>
+#include <Eigen/Dense>
+#include <Eigen/Sparse>
+#include <array>
 
+#include "setup.h"
+SetUp::SetUp(std::string file_name) {
+    read_settings(file_name);
+    make_t();
+}
+void SetUp::read_file(std::string settings_file_name) {
+    std::ifstream read_file("set.txt");
+    if (!read_file.is_open()) {
+        std::cout << "Error opening file." << std::endl;
+    }
+    read_file >> method;
+    read_file >> t_0;
+    read_file >> y_0;
+    read_file >> N;
+    t.resize(N+1);
+    y.resize(N);
+
+    read_file >> dt;
+    read_file >> sampling_frequency;
+    read_file >> polynomial_degree;
+
+    poly_coefs_y.resize(polynomial_degree+1);
+    for (int i=0; i<(polynomial_degree + 1); i++){
+        read_file >> poly_coefs_y[i];
+    }
+
+    read_file >> console_output;
+    read_file >> output_path;
+    read_file >> testing;
+    read_file.close();
+
+}
+void SetUp::read_console() {
+    char answer_test;
+    char answer_output_format;
+    std::cout<<poly_coefs_y<<std::endl;
+    std::cout<<"Enter the method: ";
+    std::cin>> method;
+
+    std::cout<<"Enter initial time t0 = ";
+    std::cin>> t_0;
+
+    std::cout<<"Enter initial y value y0 = ";
+    std::cin>> y_0;
+
+    std::cout<<"Enter total number of steps N = ";
+    std::cin>> N;
+
+    std::cout<<"Enter time step dt = ";
+    std::cin>> dt;
+
+    std::cout<<"Enter sampling frequency (integer) = ";
+    std::cin>> sampling_frequency;
+
+    std::cout<<"Enter the degree of the polynomial (integer) = ";
+    std::cin>> polynomial_degree;
+
+    for (int i=0; i<(polynomial_degree + 1); i++){
+        std::cout<<"Enter the coefficient of y^"<<  i << " = ";
+        std::cin>> poly_coefs_y[i];
+    }
+
+    std::cout<<"Do you want to se the solution in the console? - enter y (yes) or n (no): ";
+    std::cin>> answer_output_format;
+    if (answer_output_format == 'y'){ console_output = true;}
+    else { console_output = false;}
+
+    if (console_output == true){
+        std::cout<<"Enter output path: ";
+        std::cin>> output_path;
+    }
+    std::cout<<"Do you want to test? - enter y (yes) or n (no): ";
+    std::cin>> answer_test;
+    if (answer_test == 'y'){ testing = true;}
+    else { testing = false;}
+
+}
+void SetUp::read_settings(std::string settings_file_name) {
+    if (settings_file_name == ""){
+        std::cout<<"reading console...\n";
+        read_console();
+    }
+    else{
+        std::cout<<"reading file...\n";
+        read_file(settings_file_name);
+    }
+}
+
+double SetUp::RHS(double y_value, double t_value, double x_value) {
+    double rhs = 0;
+    for (int i=0; i<(polynomial_degree + 1); i++){
+        rhs += pow(y_value, i) * poly_coefs_y[i];
+    }
+    return rhs;
+}
+void SetUp::make_t() {
+
+    t[0] = t_0;
+    for (int i=0; i<(N); i++){
+        t[i+1] = (i+1)*dt;
+    }
+}
