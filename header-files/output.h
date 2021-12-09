9bccdd8be32481ebf3191335d0982e0773796b7c
AnnaPaulish <apaulish99@gmail.com>
2021-12-09 17:02:42 +0100

    output update

--- header-files/output.h
+++ header-files/output.h
@@ -1,8 +1,28 @@
-//
-// Created by Leonhard Driever on 03.12.21.
-//
+
 
 #ifndef ODE_SOLVER_PROJECT_WRITE_OUTPUT_CLASS_H
 #define ODE_SOLVER_PROJECT_WRITE_OUTPUT_CLASS_H
+
+#include "setup.h"
+#include "string"
+#include <Eigen/Dense>
+namespace E = Eigen;
+
+class Output {
+public:
+    // Constructor and destructor
+    Output(SetUp user_setup);
+
+private:
+    bool console_output;
+    E::ArrayXd y;
+    E::ArrayXd t;
+
+    void write_file(std::string path);
+    void write_screen();
+
+
+};
+
 
 #endif //ODE_SOLVER_PROJECT_WRITE_OUTPUT_CLASS_H
--- source-files/output.cpp
+++ source-files/output.cpp
@@ -1,4 +1,47 @@
-//
-// Created by Leonhard Driever on 03.12.21.
-//
 
+
+#include <iostream>
+#include <fstream>
+#include "output.h"
+#include "setup.h"
+
+Output::Output(SetUp user_setup) {
+    console_output = user_setup.console_output;
+    t = user_setup.t;
+    y = user_setup.y;
+    if (console_output){
+        write_screen();
+    }
+    else{
+        write_file(user_setup.output_path);
+    }
+
+}
+
+void Output::write_file(std::string path){
+    std::ofstream output_file(path);
+    // Check that file is properly openened.
+    if (!output_file.is_open()) {
+        std::cout << "Error opening output file." << std::endl;
+    }
+    output_file.setf(std::ios::scientific);
+    output_file.setf(std::ios::showpos);
+    output_file.precision(2);
+    // Write x vector ending with a new line
+    for (int i = 0; i < 4; ++i) {
+        output_file << y[i] << " ";
+    }
+    output_file << "\n";
+    // Flush after writing vector y
+    output_file.flush();
+    output_file.close();
+
+
+}
+void Output::write_screen() {
+    std::cout << "Solution of ODE: \n";
+    for (int i=0;i<=y.size();i++){
+        std::cout << "y["<<i<<"] = "<<y[i]<<std::endl;
+    }
+
+}
