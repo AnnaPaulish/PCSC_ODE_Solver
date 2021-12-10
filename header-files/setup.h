91f92fc1a526814740f4d4177c6a304e6ec5ac54
AnnaPaulish <apaulish99@gmail.com>
2021-12-10 08:36:12 +0100

    update

--- header-files/setup.h
+++ header-files/setup.h
@@ -8,33 +8,42 @@ namespace E = Eigen;
 
 class SetUp {
 public:
-    // Constructor and destructor
-    Set(std::string file_name);
+    
+#ifndef ODE_SOLVER_PROJECT_SETUP_H
+#define ODE_SOLVER_PROJECT_SETUP_H
+
+#include "string"
+#include <Eigen/Dense>
+namespace E = Eigen;
+
+class SetUp {
+public:
+    SetUp();
 
     int N = 5; // total number of steps N
     double dt = 0.1; // time step dt
+    double x = 0;
 
     E::ArrayXd t = E::ArrayXd::LinSpaced(N+1, 0, N*dt);
     E::ArrayXd y = E::ArrayXd::Ones(N);
 
-    int polynomial_degree = 1; 
+    int polynomial_degree = 1; //
     E::ArrayXd poly_coefs_y = E::ArrayXd::Ones(polynomial_degree+1);
 
     int sampling_frequency = 2;
 
-    double RHS(double y_value, double t_value, double x_value);
-
-    std::string output_path; // Output File Path: path/my_output.dat
+    std::string output_path = ""; // Output File Path: path/my_output.dat
     bool console_output; // True if we want to get the result in Console
+    //bool console_input;
     std::string method; // solving method (string without quotation marks)
 
     bool testing; // True if we want to test
-
     // methods
-    void read_settings(std::string settings_file_name);
+    void read_settings();
     void read_command_line();
     void read_file(std::string settings_file_name);
     void read_console();
+    double RHS(double y_value, double t_value, double x_value);
 
 private:
     double t_0; // initial time
@@ -43,6 +52,10 @@ private:
     void make_t();
 
 };
+
+
+
+#endif //ODE_SOLVER_PROJECT_SETUP_H
 
 
 
